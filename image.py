6a1bccd49ca90b6d313634b09bcc6630442d7147
Hemanthkalagar <153287858+Hemanthkalagar@users.noreply.github.com>
2026-01-29 14:49:25 +0530

    Change template from index.html to image.html

--- image.py
+++ image.py
@@ -73,7 +73,7 @@ Resume Text:
                     "suggestions": ["Unable to analyze resume properly"]
                 }
 
-    return render_template("index.html", result=result)
+    return render_template("image.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
